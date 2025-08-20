f24f0f4a8856a5cd3af592e0e216013b93672988
Bryyyan93 <brayanpalacios93@gmail.com>
2025-08-20 19:45:32 +0200

    feat: añadir test y workflow para github

---
+++ tests/test_main_api.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from app.main import app
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+from ssh import utils, api_petitions as api
+
+client = TestClient(app)
+
+# Test de root
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello"}
+
+##############################################################
+# GUI_API
+##############################################################
+# Obtener tags de las instalaciones
